8c353037bc1005d342b275387dc35d9e2111bd31
brubs09 <brunamenina632009@gmail.com>
2025-06-02 18:24:11 +0000

    exemplos da aula.

---
+++ exemplo2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    int numero;
+
+    do {
+
+        printf("Digiteum numero par para sair do programa....\n");
+        scanf("%d", &numero);
+
+        if(numero % 2 == 0){
+            printf("%d é par!\n", numero);
+         } else {
+            printf("%d é impar!\n", numero);
+
+         }
+
+    } while (numero % 2 != 0);
+
+    printf("Voce digitou um numero par, saindo do programa...\n");
+
+    return 0;
+
+}
